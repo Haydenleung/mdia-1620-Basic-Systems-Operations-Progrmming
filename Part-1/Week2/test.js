19c508fe9eec377b728d593eef770c44be41b35c
TREE HAYDEN <tree@TREEs-MacBook-Pro.local>
2022-09-16 14:45:34 -0700

    new code

---
+++ Part-1/Week2/test.js
@@ -0,0 +1,10 @@
+// Declare
+// Declare a variable called electronics devices
+// camdel case
+var eletronicDevices;
+
+// pascal case
+var ElectronicDevices;
+
+// snake case
+var Electronic_Devices;
